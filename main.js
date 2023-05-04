52c28dd3e76250920f97b27ca0a72f0efd1533c8
TheoThibodeau <teddythibodeau@gmail.com>
2023-05-04 11:15:25 -0400

    audioplayer working

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
 let mainContainer = document.querySelector("#mainContainer");
 console.log(mainContainer);
+let songsDiv = document.querySelector("#songs");
+let playSong = document.querySelector("songPreview.src");
+let searchButton = document.querySelector("#searchButton");
+let audioTag = document.querySelector("#audioTag");
 
 let url = "https://proxy-itunes-api.glitch.me/search?term=";
-
-// function getBaseApi(url) {
-
+    
     searchButton.addEventListener('click', (event) => {
+        console.log(searchButton)
         event.preventDefault()
         let searchField = document.querySelector("#searchField")
         url += searchField.value
-        console.log(url)
-        
+        console.log(url)    
+           
+
         fetch(url, {
             method: "GET",
             headers: { "Content-Type": "application/json" },
         })
-        
         .then((response) => {
             return response.json();
         })
-        
-        .then(function (data){
-            console.log(data)
+        .then ((data) => {
+            let songs = data.results;
+            console.log(data);
             
-        });
-        
+            //loop
+            for (let song of songs) {
+                //Create Elements
+                let songEl = document.createElement("div");
+                let artworkImg = document.createElement("img");
+                let songPreview = document.createElement("a")
+                
+                //Create sytling classes
+                artworkImg.classList.add("artworkImg")
+                songPreview.classList.add("songPreview")
+                songEl.classList.add("card")
 
-        // outPut.value += button.innerText
-        // problem += event.target.innerText
-        // outPut.value = problem;
-});
+                //Display elements of songs
+                artworkImg.src = song.artworkUrl60
+                songPreview.href = song.previewUrl
+                songPreview.innerText = song.trackName
+                
+                //Append Children
+                songsDiv.appendChild(songPreview)
+                songEl.appendChild(artworkImg)
+                songsDiv.appendChild(songEl)
+                mainContainer.appendChild(playButton)
+
+                //Event Listener
+            songPreview.addEventListener('click', (event) => {
+                event.preventDefault()
+                audioTag.src = event.target.href
+        })
+            }
+        })
+           });    
